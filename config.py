1359cd73b3c48d8c4cde81cdf17e54ae23346ef2
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-03 14:45:16 +0300

    set the base url and api_key configure app request.

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
 
 class Config:
     """Parent configuration class"""
 
-    pass
+    NEWS_API_BASE_URL = "https://newsapi.org/v2/top-headlines?country=ke&api_Key={}"
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class prodConfig(Config):
