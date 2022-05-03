99a8d9cdf51a533d3846c3eb4ba3ec488b897fef
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-03 12:38:32 +0300

    initialize the application, create app configurations, register blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,18 @@
-from ensurepip import bootstrap
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
+
+# Initializing the application
+def create_app(config_name):
+    app = Flask(__name__)
+    
+    # Creating the app configs
+    app.config.from_object(config_options[config_name])
+    
+    # Register the Blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
